e8e6a0f1e30faca68b473e1e47d20c31a324602e
Avi <akselrodavi@gmail.com>
2021-09-22 18:01:24 +0300

    First Commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import flask
+import json
+from flask import request, jsonify
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+x = '{ "name":"John", "age":30, "city":"New York"}'
+
+@app.route('/', methods=['GET'])
+def home():
+    return "You have 2 functions: getX and getAge"
+
+# A route to return all of the available entries in our catalog.
+@app.route('/api/v1/getX', methods=['GET'])
+def getx():
+    return jsonify(x)
+
+@app.route('/api/v1/getAge', methods=['GET'])
+def getName():
+    return jsonify(json.loads(x)["age"])
+
+#app.run(host="localhost", port=8080, debug=True,ssl_context=('cert.pem', 'key.pem'))
+app.run(host="localhost", port=8080, debug=True)
